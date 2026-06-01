5dfaad548207423f39e05229ded8b68d74bb002a
mato <ily4368@gmail.com>
2026-06-01 23:38:14 +0300

    Deepl-api: translate by chunks (50 items per request limit)

--- src/steps/store/service/translate.service/deepl/deepl-api.ts
+++ src/steps/store/service/translate.service/deepl/deepl-api.ts
@@ -1,5 +1,5 @@
 import { DeepLClient, type TargetLanguageCode } from "deepl-node";
-import { Effect, Either, Schema } from "effect";
+import { Chunk, Effect, Either, Schedule, Schema, Sink, Stream } from "effect";
 
 import { TranslateError } from "../../../error";
 import { EN, type TranslateFromLocale, type TranslateToLocale } from "../../../locales";
@@ -24,13 +24,67 @@ export const deepl: TranslateApi = ({ list, from, to, translateApiOptions }) =>
     const { apiKey, ...props } = options.right;
 
     const client = new DeepLClient(apiKey);
-    const result = yield* Effect.tryPromise({
-      try: () =>
-        client.translateText(list, from, deeplLocale[to] || "en-US", {
-          ...props,
-        }),
-      catch: (error) => new TranslateError(`Failed to translate ${String(error)}`, error),
+
+    const result = yield* translateByChunks({
+      list,
+      maxCost: 50,
+      getItemConst: () => 1,
+      callTranslation: async (list) =>
+        (
+          await client.translateText(list, from, deeplLocale[to] || "en-US", {
+            ...props,
+          })
+        ).map((item) => item.text),
     });
 
-    return result.map((item) => item.text);
+    return result;
+  });
+
+const translateByChunks = ({
+  list,
+  maxCost,
+  callTranslation,
+  getItemConst,
+}: {
+  list: string[];
+  maxCost: number;
+  callTranslation: (list: string[]) => Promise<string[]>;
+  getItemConst: (item: string) => number;
+}) =>
+  Effect.gen(function* () {
+    const stream = Stream.fromIterable(list).pipe(
+      Stream.transduce(
+        Sink.foldWeighted({
+          initial: Chunk.empty<string>(),
+          maxCost,
+          cost: (_, fragment) => getItemConst(fragment),
+          body: (group, fragment) => Chunk.append(group, fragment),
+        }),
+      ),
+      Stream.zipWithIndex,
+      Stream.mapEffect(([value, index]) =>
+        Effect.gen(function* () {
+          const list: string[] = [];
+          let totalCharCount = 0;
+          Chunk.forEach(value, (fragment) => {
+            list.push(fragment);
+            totalCharCount += fragment.length;
+          });
+          yield* Effect.logDebug(
+            `                [${index + 1}] Translating chunk of ${totalCharCount} chars...`,
+          );
+
+          const result = yield* Effect.tryPromise({
+            try: () => callTranslation(list),
+            catch: (error) => new TranslateError(`Failed to translate ${String(error)}`, error),
+          });
+
+          return Chunk.fromIterable(result);
+        }),
+      ),
+      Stream.schedule(Schedule.spaced(100)),
+      Stream.flattenChunks,
+    );
+
+    return Chunk.toArray(yield* Stream.runCollect(stream));
   });
--- src/steps/utils/clean-files.ts
+++ src/steps/utils/clean-files.ts
@@ -1,7 +1,7 @@
 import { FileSystem } from "@effect/platform";
 import { Effect } from "effect";
 
-import { PARSED_DIR, RAW_DIR, TRANSLATED_DIR, TRANSLATION_LIST_RAW } from "../store/const";
+import { PARSED_DIR, RAW_DIR, TRANSLATED_DIR } from "../store/const";
 import { TranslateStore } from "../store/store";
 
 export const cleanFiles = Effect.gen(function* () {
@@ -12,5 +12,4 @@ export const cleanFiles = Effect.gen(function* () {
   yield* fs.remove(RAW_DIR, { recursive: true });
   yield* fs.remove(PARSED_DIR, { recursive: true });
   yield* fs.remove(TRANSLATED_DIR, { recursive: true });
-  yield* fs.remove(TRANSLATION_LIST_RAW, { recursive: true });
 }).pipe(Effect.catchAll(() => Effect.log("Failed to clean tmp folder :(")));
